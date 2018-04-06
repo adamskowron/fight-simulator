2da2e3b4a1d2d0c4e7b6c829da87e782ac1f6d3c
Adam Skowronski <236036@student.pwr.edu.pl>
2018-04-07 01:41:48 +0200

    Program napisany podczas pierwszych zajęć

---
+++ lab1/zajęcia/AdamSkowronskiLab1/AdamSkowronskiLab/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace AdamSkowronskiLab
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.CollorButton = new System.Windows.Forms.Button();
+            this.timerColor = new System.Windows.Forms.Timer(this.components);
+            this.arg1TextBox1 = new System.Windows.Forms.TextBox();
+            this.arg2TextBox = new System.Windows.Forms.TextBox();
+            this.equalLabel = new System.Windows.Forms.Label();
+            this.retTextBox = new System.Windows.Forms.TextBox();
+            this.charComboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.BackColor = System.Drawing.SystemColors.Info;
+            this.NameLabel.Font = new System.Drawing.Font("Mistral", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.NameLabel.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.NameLabel.Location = new System.Drawing.Point(151, 240);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(355, 67);
+            this.NameLabel.TabIndex = 0;
+            this.NameLabel.Text = "Adam Skowronski";
+            // 
+            // CollorButton
+            // 
+            this.CollorButton.Location = new System.Drawing.Point(612, 240);
+            this.CollorButton.Name = "CollorButton";
+            this.CollorButton.Size = new System.Drawing.Size(122, 84);
+            this.CollorButton.TabIndex = 1;
+            this.CollorButton.Text = "Change Collor";
+            this.CollorButton.UseVisualStyleBackColor = true;
+            this.CollorButton.Click += new System.EventHandler(this.collorButton_Click);
+            // 
+            // timerColor
+            // 
+            this.timerColor.Tick += new System.EventHandler(this.timerColor_Tick);
+            // 
+            // arg1TextBox1
+            // 
+            this.arg1TextBox1.Location = new System.Drawing.Point(140, 110);
+            this.arg1TextBox1.Name = "arg1TextBox1";
+            this.arg1TextBox1.Size = new System.Drawing.Size(100, 26);
+            this.arg1TextBox1.TabIndex = 2;
+            this.arg1TextBox1.Text = "0";
+            this.arg1TextBox1.TextChanged += new System.EventHandler(this.arg1TextBox1_TextChanged);
+            // 
+            // arg2TextBox
+            // 
+            this.arg2TextBox.Location = new System.Drawing.Point(373, 110);
+            this.arg2TextBox.Name = "arg2TextBox";
+            this.arg2TextBox.Size = new System.Drawing.Size(100, 26);
+            this.arg2TextBox.TabIndex = 4;
+            this.arg2TextBox.Text = "0";
+            this.arg2TextBox.TextChanged += new System.EventHandler(this.arg2TextBox_TextChanged);
+            // 
+            // equalLabel
+            // 
+            this.equalLabel.AutoSize = true;
+            this.equalLabel.Location = new System.Drawing.Point(479, 108);
+            this.equalLabel.Name = "equalLabel";
+            this.equalLabel.Size = new System.Drawing.Size(18, 20);
+            this.equalLabel.TabIndex = 5;
+            this.equalLabel.Text = "=";
+            this.equalLabel.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // retTextBox
+            // 
+            this.retTextBox.Location = new System.Drawing.Point(503, 110);
+            this.retTextBox.Name = "retTextBox";
+            this.retTextBox.Size = new System.Drawing.Size(100, 26);
+            this.retTextBox.TabIndex = 6;
+            this.retTextBox.TextChanged += new System.EventHandler(this.retTextBox_TextChanged);
+            // 
+            // charComboBox
+            // 
+            this.charComboBox.FormattingEnabled = true;
+            this.charComboBox.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "/"});
+            this.charComboBox.Location = new System.Drawing.Point(246, 108);
+            this.charComboBox.Name = "charComboBox";
+            this.charComboBox.Size = new System.Drawing.Size(121, 28);
+            this.charComboBox.TabIndex = 7;
+            this.charComboBox.SelectedIndexChanged += new System.EventHandler(this.charComboBox_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(852, 408);
+            this.Controls.Add(this.charComboBox);
+            this.Controls.Add(this.retTextBox);
+            this.Controls.Add(this.equalLabel);
+            this.Controls.Add(this.arg2TextBox);
+            this.Controls.Add(this.arg1TextBox1);
+            this.Controls.Add(this.CollorButton);
+            this.Controls.Add(this.NameLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.Button CollorButton;
+        private System.Windows.Forms.Timer timerColor;
+        private System.Windows.Forms.TextBox arg1TextBox1;
+        private System.Windows.Forms.TextBox arg2TextBox;
+        private System.Windows.Forms.Label equalLabel;
+        private System.Windows.Forms.TextBox retTextBox;
+        private System.Windows.Forms.ComboBox charComboBox;
+    }
+}
+
---
+++ lab1/zajęcia/AdamSkowronskiLab1/AdamSkowronskiLab/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdamSkowronskiLab
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void collorButton_Click(object sender, EventArgs e)
+        {
+            
+
+            for (int i = 0 ; i < 10 ; i++)
+            {
+                
+                NameLabel.BackColor = Color.Brown;
+                Application.DoEvents();
+                Thread.Sleep(100);
+                NameLabel.BackColor = Color.Blue;
+                Application.DoEvents();
+                timerColor.Start();
+                CollorButton.BackColor = Color.Cyan;
+                Application.DoEvents();
+                Thread.Sleep(100);
+                CollorButton.BackColor = Color.Red;
+                Application.DoEvents();
+                Thread.Sleep(100);
+                this.BackColor = Color.Cyan;
+                Application.DoEvents();
+                Thread.Sleep(100);
+                this.BackColor = Color.Yellow;
+                Application.DoEvents();
+            }
+
+            MessageBox.Show("zakonczono migawke");
+        }
+
+        private void timerColor_Tick(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void arg1TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void charComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            string outstr;
+            int retint;
+
+            if (charComboBox.Text == "+") 
+            {
+                string arg1str = arg1TextBox1.Text;
+                int arg1int = Int32.Parse(arg1str);
+
+                string arg2str = arg2TextBox.Text;
+                int arg2int = Int32.Parse(arg2str);
+
+                retint = arg1int + arg2int;
+                outstr = retint.ToString();
+                retTextBox.Text = outstr;
+            }
+           else if (charComboBox.Text == "-")
+            {
+                string arg1str = arg1TextBox1.Text;
+                int arg1int = Int32.Parse(arg1str);
+
+                string arg2str = arg2TextBox.Text;
+                int arg2int = Int32.Parse(arg2str);
+
+                retint = arg1int - arg2int;
+                outstr = retint.ToString();
+                retTextBox.Text = outstr;
+            }
+
+           else if (charComboBox.Text == "*")
+            {
+                string arg1str = arg1TextBox1.Text;
+                int arg1int = Int32.Parse(arg1str);
+
+                string arg2str = arg2TextBox.Text;
+                int arg2int = Int32.Parse(arg2str);
+
+                retint = arg1int * arg2int;
+                outstr = retint.ToString();
+                retTextBox.Text = outstr;
+            }
+
+           else if (charComboBox.Text == "/")
+            {
+                string arg1str = arg1TextBox1.Text;
+                int arg1int = Int32.Parse(arg1str);
+
+                string arg2str = arg2TextBox.Text;
+                int arg2int = Int32.Parse(arg2str);
+
+                if (arg2int == 0)
+                {
+                    retTextBox.Text = "error";
+                }
+                else
+                {
+                    retint = arg1int / arg2int;
+                    outstr = retint.ToString();
+                    retTextBox.Text = outstr;
+                }
+            }
+
+        }
+
+        private void retTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void arg2TextBox_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
