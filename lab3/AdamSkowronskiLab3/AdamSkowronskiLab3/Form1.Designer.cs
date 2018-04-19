2094dc1c78f1fd1e401dc8e40ce9e19b30739bb4
Adam Skowronski <236036@student.pwr.edu.pl>
2018-04-19 21:10:02 +0200

    Kod napisany podczas 3 laboratorium

---
+++ lab3/AdamSkowronskiLab3/AdamSkowronskiLab3/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace AdamSkowronskiLab3
+{
+    partial class mainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelName = new System.Windows.Forms.Label();
+            this.dataGridViewData = new System.Windows.Forms.DataGridView();
+            this.buttonShow = new System.Windows.Forms.Button();
+            this.textBoxFilter = new System.Windows.Forms.TextBox();
+            this.buttonFilter = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(64, 59);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(136, 20);
+            this.labelName.TabIndex = 0;
+            this.labelName.Text = "Adam Skowronski";
+            // 
+            // dataGridViewData
+            // 
+            this.dataGridViewData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewData.Location = new System.Drawing.Point(68, 112);
+            this.dataGridViewData.Name = "dataGridViewData";
+            this.dataGridViewData.RowTemplate.Height = 28;
+            this.dataGridViewData.Size = new System.Drawing.Size(954, 280);
+            this.dataGridViewData.TabIndex = 1;
+            // 
+            // buttonShow
+            // 
+            this.buttonShow.Location = new System.Drawing.Point(1101, 202);
+            this.buttonShow.Name = "buttonShow";
+            this.buttonShow.Size = new System.Drawing.Size(147, 94);
+            this.buttonShow.TabIndex = 2;
+            this.buttonShow.Text = "pokaz";
+            this.buttonShow.UseVisualStyleBackColor = true;
+            this.buttonShow.Click += new System.EventHandler(this.buttonShow_Click);
+            // 
+            // textBoxFilter
+            // 
+            this.textBoxFilter.Location = new System.Drawing.Point(1101, 320);
+            this.textBoxFilter.Name = "textBoxFilter";
+            this.textBoxFilter.Size = new System.Drawing.Size(147, 26);
+            this.textBoxFilter.TabIndex = 3;
+            // 
+            // buttonFilter
+            // 
+            this.buttonFilter.Location = new System.Drawing.Point(1101, 368);
+            this.buttonFilter.Name = "buttonFilter";
+            this.buttonFilter.Size = new System.Drawing.Size(147, 64);
+            this.buttonFilter.TabIndex = 4;
+            this.buttonFilter.Text = "Filtruj";
+            this.buttonFilter.UseVisualStyleBackColor = true;
+            this.buttonFilter.Click += new System.EventHandler(this.buttonFilter_Click);
+            // 
+            // mainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1335, 554);
+            this.Controls.Add(this.buttonFilter);
+            this.Controls.Add(this.textBoxFilter);
+            this.Controls.Add(this.buttonShow);
+            this.Controls.Add(this.dataGridViewData);
+            this.Controls.Add(this.labelName);
+            this.Name = "mainWindow";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.DataGridView dataGridViewData;
+        private System.Windows.Forms.Button buttonShow;
+        private System.Windows.Forms.TextBox textBoxFilter;
+        private System.Windows.Forms.Button buttonFilter;
+    }
+}
+
---
+++ lab3/AdamSkowronskiLab3/AdamSkowronskiLab3/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdamSkowronskiLab3
+{
+    public partial class mainWindow : Form
+    {
+        SqlConnection connection;
+        public mainWindow()
+        {
+            InitializeComponent();
+            connection = new SqlConnection(@"Data Source = DESKTOP-NBRHJEK;database = Pizzeria; Trusted_Connection=yes");
+
+        }
+
+        private void buttonShow_Click(object sender, EventArgs e)
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM pizzas",connection);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+            dataGridViewData.DataSource = table;
+        }
+
+        private void buttonFilter_Click(object sender, EventArgs e)
+        {
+
+            SqlDataAdapter adapter = new SqlDataAdapter($"SELECT * FROM pizzas WHERE Price > 0{textBoxFilter.Text}", connection);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+            dataGridViewData.DataSource = table;
+        }
+    }
+}
